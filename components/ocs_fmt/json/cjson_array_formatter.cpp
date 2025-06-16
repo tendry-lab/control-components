1093669a62f7ddb1be9c2e298e6723146e2093a2
Dmitriy Shilin <dshil@fastmail.com>
2025-06-16 09:40:01 +0200

    Rename license header

--- components/ocs_fmt/json/cjson_array_formatter.cpp
+++ components/ocs_fmt/json/cjson_array_formatter.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, Open Control System authors
+ * Copyright (c) 2024, Tendry Lab authors
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
