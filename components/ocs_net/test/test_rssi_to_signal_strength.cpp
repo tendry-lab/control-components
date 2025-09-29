1a0b74af2b58637c0056fc105d0231a39cc7743a
Dmitriy Shilin <dshil@fastmail.com>
2025-09-29 12:51:26 +0200

    Fix RSSI unit tests

--- components/ocs_net/test/test_rssi_to_signal_strength.cpp
+++ components/ocs_net/test/test_rssi_to_signal_strength.cpp
@@ -14,23 +14,23 @@ namespace ocs {
 namespace net {
 
 TEST_CASE("Format RSSI string", "[ocs_net], [rssi_to_signal_strength]") {
-    TEST_ASSERT_EQUAL_STRING("excellent", rssi_to_signal_strength(-49));
+    TEST_ASSERT_EQUAL_STRING("Excellent", rssi_to_signal_strength(-49));
 
-    TEST_ASSERT_EQUAL_STRING("good", rssi_to_signal_strength(-50));
-    TEST_ASSERT_EQUAL_STRING("good", rssi_to_signal_strength(-51));
-    TEST_ASSERT_EQUAL_STRING("good", rssi_to_signal_strength(-59));
+    TEST_ASSERT_EQUAL_STRING("Good", rssi_to_signal_strength(-50));
+    TEST_ASSERT_EQUAL_STRING("Good", rssi_to_signal_strength(-51));
+    TEST_ASSERT_EQUAL_STRING("Good", rssi_to_signal_strength(-59));
 
-    TEST_ASSERT_EQUAL_STRING("fair", rssi_to_signal_strength(-60));
-    TEST_ASSERT_EQUAL_STRING("fair", rssi_to_signal_strength(-62));
-    TEST_ASSERT_EQUAL_STRING("fair", rssi_to_signal_strength(-69));
+    TEST_ASSERT_EQUAL_STRING("Fair", rssi_to_signal_strength(-60));
+    TEST_ASSERT_EQUAL_STRING("Fair", rssi_to_signal_strength(-62));
+    TEST_ASSERT_EQUAL_STRING("Fair", rssi_to_signal_strength(-69));
 
-    TEST_ASSERT_EQUAL_STRING("poor", rssi_to_signal_strength(-70));
-    TEST_ASSERT_EQUAL_STRING("poor", rssi_to_signal_strength(-73));
-    TEST_ASSERT_EQUAL_STRING("poor", rssi_to_signal_strength(-79));
+    TEST_ASSERT_EQUAL_STRING("Poor", rssi_to_signal_strength(-70));
+    TEST_ASSERT_EQUAL_STRING("Poor", rssi_to_signal_strength(-73));
+    TEST_ASSERT_EQUAL_STRING("Poor", rssi_to_signal_strength(-79));
 
-    TEST_ASSERT_EQUAL_STRING("unreliable", rssi_to_signal_strength(-80));
-    TEST_ASSERT_EQUAL_STRING("unreliable", rssi_to_signal_strength(-84));
-    TEST_ASSERT_EQUAL_STRING("unreliable", rssi_to_signal_strength(-101));
+    TEST_ASSERT_EQUAL_STRING("Unreliable", rssi_to_signal_strength(-80));
+    TEST_ASSERT_EQUAL_STRING("Unreliable", rssi_to_signal_strength(-84));
+    TEST_ASSERT_EQUAL_STRING("Unreliable", rssi_to_signal_strength(-101));
 }
 
 } // namespace net
