735118f8a29fd48e91536e1be17bef8bc1b9a6a5
Dmitriy Shilin <dshil@fastmail.com>
2025-10-07 17:00:18 +0200

    Introduce LED reaction on system events
    - Blink 3 times on system initialization
    - Blink 1 time when button is pressed

--- components/ocs_control/system_fsm.cpp
+++ components/ocs_control/system_fsm.cpp
@@ -7,6 +7,7 @@
  */
 
 #include "ocs_control/system_fsm.h"
+#include "ocs_control/led_task.h"
 
 #define EVENT_BITS_BUTTON_PRESSED BIT0
 #define EVENT_BITS_ALL (EVENT_BITS_BUTTON_PRESSED)
@@ -14,17 +15,29 @@
 namespace ocs {
 namespace control {
 
-SystemFsm::SystemFsm(system::IRebooter& rebooter, core::Time release_interval)
+SystemFsm::SystemFsm(system::IRebooter& rebooter,
+                     scheduler::ITaskScheduler& task_scheduler,
+                     ILED& led,
+                     core::Time release_interval)
     : release_interval_(release_interval)
-    , rebooter_(rebooter) {
+    , rebooter_(rebooter)
+    , task_scheduler_(task_scheduler)
+    , led_(led) {
 }
 
 status::StatusCode SystemFsm::run() {
-    const EventBits_t bits =
-        xEventGroupWaitBits(event_group_.get(), EVENT_BITS_ALL, pdTRUE, pdFALSE, 0);
+    switch (state_) {
+    case State::Init:
+        handle_state_init_();
+        break;
 
-    if (bits & EVENT_BITS_BUTTON_PRESSED) {
-        rebooter_.reboot();
+    case State::WaitReset:
+        handle_state_wait_reset_();
+        break;
+
+    case State::WaitInitDone:
+    case State::WaitResetDone:
+        break;
     }
 
     return status::StatusCode::OK;
@@ -43,6 +56,72 @@ status::StatusCode SystemFsm::handle_pressed(core::Time duration) {
     }
 
     return status::StatusCode::OK;
+}
+
+status::StatusCode SystemFsm::handle_event() {
+    remove_led_task_();
+
+    switch (state_) {
+    case State::WaitInitDone:
+        state_ = State::WaitReset;
+        break;
+
+    case State::WaitResetDone:
+        rebooter_.reboot();
+        break;
+
+    default:
+        //! Invalid state.
+        configASSERT(false);
+        break;
+    }
+
+    return status::StatusCode::OK;
+}
+
+void SystemFsm::handle_state_init_() {
+    add_led_task_(led_flip_count_init_);
+
+    state_ = State::WaitInitDone;
+}
+
+void SystemFsm::handle_state_wait_reset_() {
+    if (button_pressed_()) {
+        add_led_task_(led_flip_count_button_pressed_);
+
+        state_ = State::WaitResetDone;
+    }
+}
+
+void SystemFsm::add_led_task_(unsigned flip_count) {
+    configASSERT(!led_task_);
+
+    led_task_.reset(new (std::nothrow)
+                        LEDTask(*this, led_, ILED::Priority::System, flip_count));
+    configASSERT(led_task_);
+
+    configASSERT(led_.try_lock(ILED::Priority::System) == status::StatusCode::OK);
+    configASSERT(led_.turn_off() == status::StatusCode::OK);
+
+    configASSERT(task_scheduler_.add(*led_task_, led_task_id_, led_task_interval_)
+                 == status::StatusCode::OK);
+}
+
+void SystemFsm::remove_led_task_() {
+    configASSERT(led_task_);
+
+    configASSERT(task_scheduler_.remove(led_task_id_) == status::StatusCode::OK);
+
+    led_task_ = nullptr;
+
+    configASSERT(led_.unlock() == status::StatusCode::OK);
+}
+
+bool SystemFsm::button_pressed_() {
+    const EventBits_t bits =
+        xEventGroupWaitBits(event_group_.get(), EVENT_BITS_ALL, pdTRUE, pdFALSE, 0);
+
+    return bits & EVENT_BITS_BUTTON_PRESSED;
 }
 
 } // namespace control
