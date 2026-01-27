01c03c12dd574cf8c4bc3fd358ae8d16e8bfce75
Dmitriy Shilin <dshil@fastmail.com>
2026-01-27 15:38:22 +0100

    Add byte writer

---
+++ components/ocs_core/byte_writer.cpp
@@ -0,0 +1,47 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Tendry Lab
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <algorithm>
+#include <cstring>
+
+#include "ocs_core/byte_writer.h"
+
+namespace ocs {
+namespace core {
+
+ByteWriter::ByteWriter(uint8_t* data, size_t size)
+    : size_(size)
+    , data_(data) {
+}
+
+size_t ByteWriter::get_len() const {
+    return offset_;
+}
+
+size_t ByteWriter::get_cap() const {
+    return size_;
+}
+
+uint8_t* ByteWriter::get_data() {
+    return data_;
+}
+
+size_t ByteWriter::write(const uint8_t* data, size_t size) {
+    const size_t ret = std::min(size, left_());
+
+    if (ret) {
+        memcpy(data_ + offset_, data, ret);
+        offset_ += ret;
+    }
+
+    return ret;
+}
+
+size_t ByteWriter::left_() const {
+    return size_ - offset_;
+}
+
+} // namespace core
+} // namespace ocs
