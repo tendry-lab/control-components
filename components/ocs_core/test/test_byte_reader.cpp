3871f02439a237bae2bbce33f468df31995694b1
Dmitriy Shilin <dshil@fastmail.com>
2026-01-30 15:59:20 +0100

    Properly return data when there is no bytes left for byte reader

--- components/ocs_core/test/test_byte_reader.cpp
+++ components/ocs_core/test/test_byte_reader.cpp
@@ -24,6 +24,9 @@ TEST_CASE("Byte reader: read array: whole at once", "[ocs_core], [byte_reader]")
 
     TEST_ASSERT_EQUAL(sizeof(write_buf), reader.read(read_buf, sizeof(read_buf)));
     TEST_ASSERT_TRUE(memcmp(write_buf, read_buf, sizeof(write_buf)) == 0);
+
+    TEST_ASSERT_EQUAL(0, reader.get_len());
+    TEST_ASSERT_NULL(reader.get_data());
 }
 
 TEST_CASE("Byte reader: read byte", "[ocs_core], [byte_reader]") {
@@ -41,6 +44,9 @@ TEST_CASE("Byte reader: read byte", "[ocs_core], [byte_reader]") {
 
     uint8_t byte = 0;
     TEST_ASSERT_FALSE(reader.read(byte));
+
+    TEST_ASSERT_EQUAL(0, reader.get_len());
+    TEST_ASSERT_NULL(reader.get_data());
 }
 
 TEST_CASE("Byte reader: read by offset: whole at once", "[ocs_core], [byte_reader]") {
@@ -61,6 +67,12 @@ TEST_CASE("Byte reader: read by offset: whole at once", "[ocs_core], [byte_reade
     TEST_ASSERT_EQUAL(sizeof(write_buf) - 1, reader.get_len());
     TEST_ASSERT_TRUE(memcmp(write_buf + 1, off_reader.get_data(), off_reader.get_len())
                      == 0);
+
+    while (off_reader.get_len()) {
+        TEST_ASSERT_TRUE(off_reader.read(byte));
+        TEST_ASSERT_EQUAL(5, byte);
+    }
+    TEST_ASSERT_NULL(off_reader.get_data());
 }
 
 TEST_CASE("Byte reader: read by offset: read less", "[ocs_core], [byte_reader]") {
@@ -96,6 +108,8 @@ TEST_CASE("Byte reader: read by offset: read less", "[ocs_core], [byte_reader]")
     TEST_ASSERT_EQUAL(0, byte);
 
     TEST_ASSERT_FALSE(off_reader.read(byte));
+    TEST_ASSERT_EQUAL(0, off_reader.get_len());
+    TEST_ASSERT_NULL(off_reader.get_data());
 }
 
 } // namespace core
