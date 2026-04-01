6bb62bcb59d2d6fcc64aa6c52b11fabd3a6d516c
Dmitriy Shilin <dshil@fastmail.com>
2026-04-01 10:30:07 +0200

    Simplify mutex

--- components/ocs_core/static_mutex.h
+++ components/ocs_core/static_mutex.h
@@ -5,20 +5,30 @@
 
 #pragma once
 
-#include "ocs_core/basic_static_mutex.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "ocs_core/ilocker.h"
 #include "ocs_core/noncopyable.h"
 
 namespace ocs {
 namespace core {
 
 //! FreeRTOS mutex to be used in FreeRTOS tasks.
-class StaticMutex : public BasicStaticMutex, private NonCopyable<> {
+class StaticMutex : public ILocker, private NonCopyable<> {
 public:
+    //! Initialize.
+    StaticMutex();
+
     //! Lock the mutex.
     status::StatusCode lock(TickType_t wait = portMAX_DELAY) override;
 
     //! Unlock the mutex.
     status::StatusCode unlock() override;
+
+private:
+    SemaphoreHandle_t sem_ { nullptr };
+    StaticSemaphore_t buff_;
 };
 
 } // namespace core
