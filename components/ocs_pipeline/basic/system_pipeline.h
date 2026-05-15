3d3b8655b60b81929ea78f77edf5a440f6007b67
Dmitriy Shilin <dshil@fastmail.com>
2026-05-15 11:02:37 +0200

    Simplify system pipeline

--- components/ocs_pipeline/basic/system_pipeline.h
+++ components/ocs_pipeline/basic/system_pipeline.h
@@ -8,7 +8,6 @@
 #include <memory>
 
 #include "ocs_core/noncopyable.h"
-#include "ocs_scheduler/async_func_scheduler.h"
 #include "ocs_scheduler/idelay_estimator.h"
 #include "ocs_scheduler/itask_scheduler.h"
 #include "ocs_status/code.h"
@@ -26,23 +25,13 @@ namespace basic {
 
 class SystemPipeline : private core::NonCopyable<> {
 public:
-    struct Params {
-        struct TaskScheduler {
-            //! Delay after all tasks have been run.
-            TickType_t delay { 0 };
-        } task_scheduler;
-    };
-
     //! Initialize.
-    explicit SystemPipeline(Params params);
+    SystemPipeline();
 
     const system::DeviceInfo& get_device_info() const;
 
     system::IClock& get_clock();
     storage::StorageBuilder& get_storage_builder();
-    scheduler::AsyncFuncScheduler& get_func_scheduler();
-    scheduler::ITaskScheduler& get_task_scheduler();
-    scheduler::ITask& get_reboot_task();
     system::IRebooter& get_rebooter();
     system::FanoutRebootHandler& get_reboot_handler();
 
@@ -52,17 +41,10 @@ private:
 
     std::unique_ptr<system::IClock> clock_;
 
-    std::unique_ptr<scheduler::IDelayEstimator> delay_estimator_;
-    std::unique_ptr<scheduler::ITaskScheduler> task_scheduler_;
-    std::unique_ptr<scheduler::AsyncFuncScheduler> func_scheduler_;
-
     std::unique_ptr<system::FanoutRebootHandler> fanout_reboot_handler_;
     system::PlatformBuilder::IRebooterPtr default_rebooter_;
     std::unique_ptr<system::IRebooter> delay_rebooter_;
     system::IRebooter* rebooter_ { nullptr };
-
-    std::unique_ptr<scheduler::ITask> reboot_task_;
-    std::unique_ptr<scheduler::ITask> reboot_task_async_;
 
     std::unique_ptr<system::DeviceInfo> device_info_;
 };
