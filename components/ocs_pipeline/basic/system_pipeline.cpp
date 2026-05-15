8d69239936c5938b4710d797bcc2dc757ac81e39
Dmitriy Shilin <dshil@fastmail.com>
2026-05-15 11:02:37 +0200

    Simplify system pipeline

--- components/ocs_pipeline/basic/system_pipeline.cpp
+++ components/ocs_pipeline/basic/system_pipeline.cpp
@@ -6,9 +6,6 @@
 #include "freertos/FreeRTOSConfig.h"
 
 #include "ocs_pipeline/basic/system_pipeline.h"
-#include "ocs_scheduler/async_func.h"
-#include "ocs_scheduler/constant_delay_estimator.h"
-#include "ocs_scheduler/periodic_task_scheduler.h"
 #include "ocs_status/code_to_str.h"
 #include "ocs_system/delay_rebooter.h"
 #include "ocs_system/reboot_task.h"
@@ -18,59 +15,26 @@ namespace ocs {
 namespace pipeline {
 namespace basic {
 
-SystemPipeline::SystemPipeline(SystemPipeline::Params params) {
-    configASSERT(params.task_scheduler.delay);
-
-    flash_initializer_.reset(new (std::nothrow) storage::FlashInitializer());
-    configASSERT(flash_initializer_);
-
-    storage_builder_.reset(new (std::nothrow) storage::StorageBuilder());
-    configASSERT(storage_builder_);
-
-    clock_.reset(new (std::nothrow) system::Clock());
-    configASSERT(clock_);
-
-    delay_estimator_.reset(new (std::nothrow) scheduler::ConstantDelayEstimator(
-        params.task_scheduler.delay));
-    configASSERT(delay_estimator_);
-
-    task_scheduler_.reset(new (std::nothrow) scheduler::PeriodicTaskScheduler(
-        *clock_, *delay_estimator_, "system_pipeline_scheduler", 16));
-    configASSERT(task_scheduler_);
-
-    func_scheduler_.reset(new (std::nothrow) scheduler::AsyncFuncScheduler(16));
-    configASSERT(func_scheduler_);
-
-    configASSERT(task_scheduler_->add(*func_scheduler_, "system_async_func_scheduler",
-                                      system::Duration::millisecond * 500)
-                 == status::StatusCode::OK);
-
-    fanout_reboot_handler_.reset(new (std::nothrow) system::FanoutRebootHandler());
-    configASSERT(fanout_reboot_handler_);
+SystemPipeline::SystemPipeline() {
+    flash_initializer_ = std::make_unique<storage::FlashInitializer>();
+    storage_builder_ = std::make_unique<storage::StorageBuilder>();
+    clock_ = std::make_unique<system::Clock>();
+    fanout_reboot_handler_ = std::make_unique<system::FanoutRebootHandler>();
 
     default_rebooter_ = system::PlatformBuilder::make_rebooter(*fanout_reboot_handler_);
-    configASSERT(default_rebooter_);
-
     rebooter_ = default_rebooter_.get();
 
-    delay_rebooter_.reset(new (std::nothrow)
-                              system::DelayRebooter(pdMS_TO_TICKS(500), *rebooter_));
-    configASSERT(delay_rebooter_);
+    delay_rebooter_ =
+        std::make_unique<system::DelayRebooter>(pdMS_TO_TICKS(500), *rebooter_);
 
     rebooter_ = delay_rebooter_.get();
 
-    reboot_task_.reset(new (std::nothrow) system::RebootTask(*rebooter_));
-    configASSERT(reboot_task_);
+    configASSERT(rebooter_);
 
-    reboot_task_async_.reset(new (std::nothrow)
-                                 scheduler::AsyncFunc(*func_scheduler_, *reboot_task_));
-    configASSERT(reboot_task_async_);
-
-    device_info_.reset(new (std::nothrow) system::DeviceInfo(
+    device_info_ = std::make_unique<system::DeviceInfo>(
         CONFIG_OCS_CORE_FW_NAME, CONFIG_OCS_CORE_FW_VERSION,
         CONFIG_OCS_CORE_FW_DESCRIPTION, CONFIG_OCS_CORE_PRODUCT_NAME,
-        CONFIG_OCS_CORE_COMPANY_UUID));
-    configASSERT(device_info_);
+        CONFIG_OCS_CORE_COMPANY_UUID);
 }
 
 system::IClock& SystemPipeline::get_clock() {
@@ -79,18 +43,6 @@ system::IClock& SystemPipeline::get_clock() {
 
 storage::StorageBuilder& SystemPipeline::get_storage_builder() {
     return *storage_builder_;
-}
-
-scheduler::AsyncFuncScheduler& SystemPipeline::get_func_scheduler() {
-    return *func_scheduler_;
-}
-
-scheduler::ITaskScheduler& SystemPipeline::get_task_scheduler() {
-    return *task_scheduler_;
-}
-
-scheduler::ITask& SystemPipeline::get_reboot_task() {
-    return *reboot_task_async_;
 }
 
 system::IRebooter& SystemPipeline::get_rebooter() {
