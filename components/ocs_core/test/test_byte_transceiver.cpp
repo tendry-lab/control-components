d1f2e7a53c6ad6b43a6e9fee0411e7b402a6cf7e
Dmitriy Shilin <dshil@fastmail.com>
2026-08-12 11:29:57 +0200

    Add test case for byte transferring of signed int value

--- components/ocs_core/test/test_byte_transceiver.cpp
+++ components/ocs_core/test/test_byte_transceiver.cpp
@@ -31,6 +31,23 @@ TEST_CASE("Test byte read/write", "[ocs_core], [byte_reader], [byte_writer]") {
 
         TEST_ASSERT_EQUAL(w_value, r_value);
     }
+    { // Check i32
+        const int32_t w_value = -0x12345678;
+
+        uint8_t w_buf[sizeof(w_value)];
+        memset(w_buf, 0, sizeof(w_buf));
+
+        ByteWriter writer(w_buf, sizeof(w_buf));
+
+        TEST_ASSERT_TRUE(writer.write(w_value));
+
+        ByteReader reader(writer.get_data(), writer.get_len());
+
+        int32_t r_value = 0;
+        TEST_ASSERT_TRUE(reader.read(r_value));
+
+        TEST_ASSERT_EQUAL_INT32(w_value, r_value);
+    }
     { // Check u24
         const uint32_t w_value = 0x123456;
 
