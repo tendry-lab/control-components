e64e85fd11c39ac462024156e8b09a03f35d69ad
Dmitriy Shilin <dshil@fastmail.com>
2026-08-14 15:09:59 +0200

    core: unify fix-width comparison in tests

--- components/ocs_core/test/test_byte_reader.cpp
+++ components/ocs_core/test/test_byte_reader.cpp
@@ -17,15 +17,15 @@ TEST_CASE("Byte reader: read array: whole at once", "[ocs_core], [byte_reader]")
     memset(write_buf, 5, sizeof(write_buf));
 
     ByteReader reader(write_buf, sizeof(write_buf));
-    TEST_ASSERT_EQUAL(sizeof(write_buf), reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(sizeof(write_buf), reader.get_len());
 
     uint8_t read_buf[sizeof(write_buf)];
     memset(read_buf, 0, sizeof(read_buf));
 
-    TEST_ASSERT_EQUAL(sizeof(write_buf), reader.read(read_buf, sizeof(read_buf)));
+    TEST_ASSERT_EQUAL_UINT32(sizeof(write_buf), reader.read(read_buf, sizeof(read_buf)));
     TEST_ASSERT_TRUE(memcmp(write_buf, read_buf, sizeof(write_buf)) == 0);
 
-    TEST_ASSERT_EQUAL(0, reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(0, reader.get_len());
     TEST_ASSERT_NULL(reader.get_data());
 }
 
@@ -34,7 +34,7 @@ TEST_CASE("Byte reader: read byte", "[ocs_core], [byte_reader]") {
     memset(write_buf, 5, sizeof(write_buf));
 
     ByteReader reader(write_buf, sizeof(write_buf));
-    TEST_ASSERT_EQUAL(sizeof(write_buf), reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(sizeof(write_buf), reader.get_len());
 
     for (size_t n = 0; n < sizeof(write_buf); ++n) {
         uint8_t byte = 0;
@@ -45,7 +45,7 @@ TEST_CASE("Byte reader: read byte", "[ocs_core], [byte_reader]") {
     uint8_t byte = 0;
     TEST_ASSERT_FALSE(reader.read(byte));
 
-    TEST_ASSERT_EQUAL(0, reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(0, reader.get_len());
     TEST_ASSERT_NULL(reader.get_data());
 }
 
@@ -54,17 +54,17 @@ TEST_CASE("Byte reader: read by offset: whole at once", "[ocs_core], [byte_reade
     memset(write_buf, 5, sizeof(write_buf));
 
     ByteReader reader(write_buf, sizeof(write_buf));
-    TEST_ASSERT_EQUAL(sizeof(write_buf), reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(sizeof(write_buf), reader.get_len());
     TEST_ASSERT_EQUAL_PTR(write_buf, reader.get_data());
 
     uint8_t byte = 0;
     TEST_ASSERT_TRUE(reader.read(byte));
     TEST_ASSERT_EQUAL_UINT8(5, byte);
-    TEST_ASSERT_EQUAL(sizeof(write_buf) - 1, reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(sizeof(write_buf) - 1, reader.get_len());
 
     ByteReader off_reader(reader.get_data(), reader.get_len());
     TEST_ASSERT_EQUAL_PTR(write_buf + 1, off_reader.get_data());
-    TEST_ASSERT_EQUAL(sizeof(write_buf) - 1, reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(sizeof(write_buf) - 1, reader.get_len());
     TEST_ASSERT_TRUE(memcmp(write_buf + 1, off_reader.get_data(), off_reader.get_len())
                      == 0);
 
@@ -80,7 +80,7 @@ TEST_CASE("Byte reader: read by offset: read less", "[ocs_core], [byte_reader]")
     memset(write_buf, 5, sizeof(write_buf));
 
     ByteReader reader(write_buf, sizeof(write_buf));
-    TEST_ASSERT_EQUAL(sizeof(write_buf), reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(sizeof(write_buf), reader.get_len());
 
     uint8_t byte = 0;
     TEST_ASSERT_TRUE(reader.read(byte));
@@ -89,12 +89,13 @@ TEST_CASE("Byte reader: read by offset: read less", "[ocs_core], [byte_reader]")
     TEST_ASSERT_TRUE(reader.read(byte));
     TEST_ASSERT_EQUAL_UINT8(5, byte);
 
-    TEST_ASSERT_EQUAL(sizeof(write_buf) - 2, reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(sizeof(write_buf) - 2, reader.get_len());
 
     uint8_t read_buf[sizeof(write_buf)];
     memset(read_buf, 0, sizeof(read_buf));
 
-    TEST_ASSERT_EQUAL(sizeof(read_buf) - 2, reader.read(read_buf, sizeof(read_buf)));
+    TEST_ASSERT_EQUAL_UINT32(sizeof(read_buf) - 2,
+                             reader.read(read_buf, sizeof(read_buf)));
     TEST_ASSERT_TRUE(memcmp(write_buf + 2, read_buf, sizeof(read_buf) - 2) == 0);
 
     TEST_ASSERT_FALSE(reader.read(byte));
@@ -108,7 +109,7 @@ TEST_CASE("Byte reader: read by offset: read less", "[ocs_core], [byte_reader]")
     TEST_ASSERT_EQUAL_UINT8(0, byte);
 
     TEST_ASSERT_FALSE(off_reader.read(byte));
-    TEST_ASSERT_EQUAL(0, off_reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(0, off_reader.get_len());
     TEST_ASSERT_NULL(off_reader.get_data());
 }
 
@@ -117,16 +118,16 @@ TEST_CASE("Byte reader: discard: less", "[ocs_core], [byte_reader]") {
     memset(write_buf, 5, sizeof(write_buf));
 
     ByteReader reader(write_buf, sizeof(write_buf));
-    TEST_ASSERT_EQUAL(sizeof(write_buf), reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(sizeof(write_buf), reader.get_len());
 
-    TEST_ASSERT_EQUAL(reader.get_len() - 1, reader.discard(reader.get_len() - 1));
-    TEST_ASSERT_EQUAL(1, reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(reader.get_len() - 1, reader.discard(reader.get_len() - 1));
+    TEST_ASSERT_EQUAL_UINT32(1, reader.get_len());
 
     uint8_t byte = 0;
     TEST_ASSERT_TRUE(reader.read(byte));
     TEST_ASSERT_EQUAL_UINT8(5, byte);
 
-    TEST_ASSERT_EQUAL(0, reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(0, reader.get_len());
 }
 
 TEST_CASE("Byte reader: discard: more", "[ocs_core], [byte_reader]") {
@@ -134,16 +135,16 @@ TEST_CASE("Byte reader: discard: more", "[ocs_core], [byte_reader]") {
     memset(write_buf, 5, sizeof(write_buf));
 
     ByteReader reader(write_buf, sizeof(write_buf));
-    TEST_ASSERT_EQUAL(sizeof(write_buf), reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(sizeof(write_buf), reader.get_len());
 
-    TEST_ASSERT_EQUAL(reader.get_len(), reader.discard(reader.get_len() + 1));
-    TEST_ASSERT_EQUAL(0, reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(reader.get_len(), reader.discard(reader.get_len() + 1));
+    TEST_ASSERT_EQUAL_UINT32(0, reader.get_len());
 
     uint8_t byte = 0;
     TEST_ASSERT_FALSE(reader.read(byte));
     TEST_ASSERT_EQUAL_UINT8(0, byte);
 
-    TEST_ASSERT_EQUAL(0, reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(0, reader.get_len());
 }
 
 TEST_CASE("Byte reader: discard: all", "[ocs_core], [byte_reader]") {
@@ -151,16 +152,16 @@ TEST_CASE("Byte reader: discard: all", "[ocs_core], [byte_reader]") {
     memset(write_buf, 5, sizeof(write_buf));
 
     ByteReader reader(write_buf, sizeof(write_buf));
-    TEST_ASSERT_EQUAL(sizeof(write_buf), reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(sizeof(write_buf), reader.get_len());
 
-    TEST_ASSERT_EQUAL(reader.get_len(), reader.discard(reader.get_len()));
-    TEST_ASSERT_EQUAL(0, reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(reader.get_len(), reader.discard(reader.get_len()));
+    TEST_ASSERT_EQUAL_UINT32(0, reader.get_len());
 
     uint8_t byte = 0;
     TEST_ASSERT_FALSE(reader.read(byte));
     TEST_ASSERT_EQUAL_UINT8(0, byte);
 
-    TEST_ASSERT_EQUAL(0, reader.get_len());
+    TEST_ASSERT_EQUAL_UINT32(0, reader.get_len());
 }
 
 } // namespace core
--- components/ocs_core/test/test_byte_writer.cpp
+++ components/ocs_core/test/test_byte_writer.cpp
@@ -17,7 +17,7 @@ TEST_CASE("Byte writer: write byte", "[ocs_core], [byte_writer]") {
     memset(write_buf, 0, sizeof(write_buf));
 
     ByteWriter writer(write_buf, sizeof(write_buf));
-    TEST_ASSERT_EQUAL(0, writer.get_len());
+    TEST_ASSERT_EQUAL_UINT32(0, writer.get_len());
 
     for (uint8_t n = 0; n < sizeof(write_buf); ++n) {
         TEST_ASSERT_TRUE(writer.write(n));
@@ -33,12 +33,12 @@ TEST_CASE("Byte writer: write array: whole at once", "[ocs_core], [byte_writer]"
     memset(write_buf, 0, sizeof(write_buf));
 
     ByteWriter writer(write_buf, sizeof(write_buf));
-    TEST_ASSERT_EQUAL(0, writer.get_len());
+    TEST_ASSERT_EQUAL_UINT32(0, writer.get_len());
 
     uint8_t buf[sizeof(write_buf)];
     memset(buf, 5, sizeof(buf));
 
-    TEST_ASSERT_EQUAL(sizeof(write_buf), writer.write(buf, sizeof(buf)));
+    TEST_ASSERT_EQUAL_UINT32(sizeof(write_buf), writer.write(buf, sizeof(buf)));
     TEST_ASSERT_TRUE(memcmp(write_buf, buf, sizeof(buf)) == 0);
 }
 
@@ -49,21 +49,21 @@ TEST_CASE("Byte writer: write array: by parts", "[ocs_core], [byte_writer]") {
     ByteWriter writer(write_buf, sizeof(write_buf));
 
     TEST_ASSERT_TRUE(writer.write(static_cast<uint8_t>(5)));
-    TEST_ASSERT_EQUAL(1, writer.get_len());
+    TEST_ASSERT_EQUAL_UINT32(1, writer.get_len());
 
     uint8_t buf[2];
     memset(buf, 3, sizeof(buf));
-    TEST_ASSERT_EQUAL(sizeof(buf), writer.write(buf, sizeof(buf)));
+    TEST_ASSERT_EQUAL_UINT32(sizeof(buf), writer.write(buf, sizeof(buf)));
 
     memset(buf, 7, sizeof(buf));
-    TEST_ASSERT_EQUAL(sizeof(buf), writer.write(buf, sizeof(buf)));
+    TEST_ASSERT_EQUAL_UINT32(sizeof(buf), writer.write(buf, sizeof(buf)));
 
     // Write the remaining byte
     TEST_ASSERT_TRUE(writer.write(static_cast<uint8_t>(1)));
-    TEST_ASSERT_EQUAL(sizeof(write_buf), writer.get_len());
+    TEST_ASSERT_EQUAL_UINT32(sizeof(write_buf), writer.get_len());
 
     uint8_t want_buf[] = { 5, 3, 3, 7, 7, 1 };
-    TEST_ASSERT_EQUAL(sizeof(want_buf), writer.get_len());
+    TEST_ASSERT_EQUAL_UINT32(sizeof(want_buf), writer.get_len());
     TEST_ASSERT_TRUE(memcmp(want_buf, writer.get_data(), sizeof(want_buf)) == 0);
 }
 
@@ -103,8 +103,8 @@ TEST_CASE("Byte writer: find byte: in range", "[ocs_core], [byte_writer]") {
     TEST_ASSERT_TRUE(writer.write_u8(0x5A));
     TEST_ASSERT_TRUE(writer.write_u8(0xA5));
 
-    TEST_ASSERT_EQUAL(4, writer.find(0x5A));
-    TEST_ASSERT_EQUAL(-1, writer.find(0x0A));
+    TEST_ASSERT_EQUAL_INT32(4, writer.find(0x5A));
+    TEST_ASSERT_EQUAL_INT32(-1, writer.find(0x0A));
 }
 
 TEST_CASE("Byte writer: find byte: out of range", "[ocs_core], [byte_writer]") {
@@ -121,11 +121,11 @@ TEST_CASE("Byte writer: find byte: out of range", "[ocs_core], [byte_writer]") {
     TEST_ASSERT_TRUE(writer.write_u8(0x5A));
     TEST_ASSERT_TRUE(writer.write_u8(0xA5));
 
-    TEST_ASSERT_EQUAL(4, writer.find(0xA5));
-    TEST_ASSERT_EQUAL(-1, writer.find(0xAA));
+    TEST_ASSERT_EQUAL_INT32(4, writer.find(0xA5));
+    TEST_ASSERT_EQUAL_INT32(-1, writer.find(0xAA));
 
     TEST_ASSERT_TRUE(writer.write_u8(0xAA));
-    TEST_ASSERT_EQUAL(5, writer.find(0xAA));
+    TEST_ASSERT_EQUAL_INT32(5, writer.find(0xAA));
 }
 
 } // namespace core
